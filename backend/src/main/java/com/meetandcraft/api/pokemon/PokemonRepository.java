b4e3f483c15c7298a2f105c9b39de5e807ba7e4a
PholAsa-Remy <pholasa.remy@outlook.fr>
2024-01-05 13:43:19 +0100

    Removing old Pokemon Api and Review Api Reference

--- backend/src/main/java/com/meetandcraft/api/pokemon/PokemonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.meetandcraft.api.pokemon;
-
-import com.meetandcraft.api.pokemon.Pokemon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
-}
--- backend/src/main/java/com/meetandcraft/api/review/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meetandcraft.api.review;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ReviewRepository extends JpaRepository<Review,Integer> {
-    List<Review> findByPokemonId (int pokemonId);
-}
